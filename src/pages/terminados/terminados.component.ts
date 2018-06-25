a372145bb53bc571e451d2131065f7f095d48090
esteban03 <esteban03@outlook.cl>
2018-06-24 21:58:00 -0400

    Se desarrollo la vista de listas terminadas

--- src/pages/terminados/terminados.component.ts
+++ src/pages/terminados/terminados.component.ts
@@ -1,4 +1,8 @@
 import { Component } from "@angular/core";
+import { ListasService } from '../../app/services/listas.services';
+import { DetalleComponent } from '../detalle/detalle.component';
+import { Lista } from '../../app/clases/index';
+import { NavController } from 'ionic-angular';
 
 @Component({
     selector: 'app-terminados',
@@ -6,7 +10,12 @@ import { Component } from "@angular/core";
 })
 export class TerminadosComponent {
     
-    constructor(){        
+    constructor(public listaDeseos: ListasService, 
+                private navCtrl: NavController) {   
+    }
+
+    irDetalle(id: number, lista: Lista) {
+        this.navCtrl.push(DetalleComponent, { id, lista });
     }
 
 }
